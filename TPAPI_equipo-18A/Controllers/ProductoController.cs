65f12036839dd793e94646a1e6b06ee34a66127b
Seremma <serg.emma@hotmail.com>
2025-10-14 23:03:23 -0300

    Validación pull a corregir marca

--- TPAPI_equipo-18A/Controllers/ProductoController.cs
+++ TPAPI_equipo-18A/Controllers/ProductoController.cs
@@ -103,22 +103,76 @@ namespace TPAPI_equipo_18A.Controllers
         }
 
         // PUT: api/Producto/5
-        public void Put(int id, [FromBody] ArticuloDto art)
+        public HttpResponseMessage Put(int id, [FromBody] ArticuloDto art)
         {
-            ArticuloNegocio negocio = new ArticuloNegocio();
-            Articulo nuevo = new Articulo();
+            try
+            {
+                if (id <= 0)
+                {
+                    return Request.CreateResponse(HttpStatusCode.BadRequest, "El ID del artículo no puede ser menor o igual a cero.");
+                }
 
-            // Asigno ID
-            nuevo.Id = id;
+                if (art == null)
+                {
+                    return Request.CreateResponse(HttpStatusCode.BadRequest, "El cuerpo de la solicitud no puede ser nulo.");
+                }
 
-            nuevo.Codigo = art.Codigo;
-            nuevo.Nombre = art.Nombre;
-            nuevo.Descripcion = art.Descripcion;
-            nuevo.Marca = new Marca { Id = art.IdMarca };
-            nuevo.Categoria = new Categoria { Id = art.IdCategoria };
-            nuevo.Precio = art.Precio;
+                // Asigno ID
 
-            negocio.modificar(nuevo);
+                ArticuloNegocio negocio = new ArticuloNegocio();
+                Articulo nuevo = new Articulo();
+                MarcasNegocio marcaNegocio = new MarcasNegocio();
+                CategoriasNegocio categoriaNegocio = new CategoriasNegocio();
+                List<Articulo> lista = negocio.listar();
+                nuevo.Id = id;
+                if (negocio.listar().Find(x => x.Id == id) == null)
+                {
+                    return Request.CreateResponse(HttpStatusCode.NotFound, "No se encontró el artículo con el ID proporcionado.");
+                }
+
+                if (string.IsNullOrEmpty(art.Codigo))
+                {
+                    return Request.CreateResponse(HttpStatusCode.BadRequest, "El código de artículo no puede ser nulo.");
+                }
+                nuevo.Codigo = art.Codigo;
+
+                if (string.IsNullOrEmpty(art.Nombre))
+                {
+                    return Request.CreateResponse(HttpStatusCode.BadRequest, "El nombre del artículo no puede ser nulo.");
+                }
+                nuevo.Nombre = art.Nombre;
+
+                if (string.IsNullOrEmpty(art.Descripcion))
+                {
+                    return Request.CreateResponse(HttpStatusCode.BadRequest, "La descripción del artículo no puede ser nula.");
+                }
+                nuevo.Descripcion = art.Descripcion;
+                nuevo.Marca = new Marca { Id = art.IdMarca };
+                nuevo.Categoria = new Categoria { Id = art.IdCategoria };
+
+                if (art.Precio <= 0)
+                {
+                    return Request.CreateResponse(HttpStatusCode.BadRequest, "El precio del artículo no puede ser menor o igual a cero.");
+                }
+                nuevo.Precio = art.Precio;
+
+                Marca marca = marcaNegocio.listar().Find(x => x.Id == art.IdMarca);
+                Categoria categoria = categoriaNegocio.listar().Find(x => x.Id == art.IdCategoria);
+
+                if (marca == null)
+                    return Request.CreateResponse(HttpStatusCode.BadRequest, "La marca no existe." + art.IdMarca );
+
+                if (categoria == null)
+                    return Request.CreateResponse(HttpStatusCode.BadRequest, "La categoría no existe.");
+
+                negocio.modificar(nuevo);
+
+                return Request.CreateResponse(HttpStatusCode.Created, "Articulo modificado.");
+            }
+            catch (Exception ex)
+            {
+                return Request.CreateResponse(HttpStatusCode.InternalServerError, "Error al modificar el producto: " + ex.Message.ToString());
+            }
         }
 
 
